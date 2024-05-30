e2a4156377569029546689e4bf01062827f44ebe
Jack Truong <jackiextruong@yahoo.com>
2024-05-30 14:50:51 -0700

    Refactor into components

---
+++ src/components/ImageControl.tsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import SingleSelectList from "./SingleSelectList.tsx";
+import AiService from "../services/AiService.ts";
+import {Box} from "@mui/material";
+
+type ImageControlProps = {
+  breed: string;
+};
+
+const ImageControl = ({breed}: ImageControlProps) => {
+  const [activity, setActivity] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+  const [environment, setEnvironment] = useState<string>("");
+  const [food, setFood] = useState<string>("");
+  const [instrument, setInstrument] = useState<string>("");
+
+  return (
+      <Box sx={{ border: 3, padding: 2 }}>
+        <h2>{"AI Image Generation"}</h2>
+        <div>{`Selected breed: ${breed}`}</div>
+        <SingleSelectList description={"Select an activity"} getValues={AiService.getActivities}
+                          setValue={setActivity}></SingleSelectList>
+        <SingleSelectList description={"Select a color"} getValues={AiService.getColors}
+                          setValue={setColor}></SingleSelectList>
+        <SingleSelectList description={"Select an environment"} getValues={AiService.getEnvironments}
+                          setValue={setEnvironment}></SingleSelectList>
+        <SingleSelectList description={"Select a food"} getValues={AiService.getFoods}
+                          setValue={setFood}></SingleSelectList>
+        <SingleSelectList description={"Select an instrument"} getValues={AiService.getInstruments}
+                          setValue={setInstrument}></SingleSelectList>
+      </Box>
+  )
+}
+
+export default ImageControl;
